16fed545a7c73c2ae3e56050abe9e3d99e755287
Henry Walshaw <henry.walshaw@gmail.com>
2018-11-20 04:00:10 +1100

    Move away from nesting to speed up queries.

--- geojson_service/serialisers.py
+++ geojson_service/serialisers.py
@@ -1,17 +1,5 @@
-from marshmallow import fields
-
 from .serializer_utils import MarshmallowGeoJSON, ma
 from .models import VotingCentre, District
-
-
-class VotingCentreTable(ma.ModelSchema):
-    class Meta:
-        model = VotingCentre
-
-
-class DistrictTable(ma.ModelSchema):
-    class Meta:
-        model = District
 
 
 class VotingCentreGJ(MarshmallowGeoJSON, ma.ModelSchema):
@@ -19,14 +7,11 @@ class VotingCentreGJ(MarshmallowGeoJSON, ma.ModelSchema):
     class Meta:
         model = VotingCentre
 
-    districts = fields.Nested(DistrictTable, only='name', many=True, dump_only=True)
-
 
 class DistrictGJ(MarshmallowGeoJSON, ma.ModelSchema):
+
     class Meta:
         model = District
-
-    voting_centres = fields.Nested(VotingCentreTable, only=('venue_name'), many=True, dump_only=True)
 
 
 voting_centre_collection = VotingCentreGJ(many=True)
