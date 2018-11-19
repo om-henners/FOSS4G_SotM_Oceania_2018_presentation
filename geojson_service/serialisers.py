3a6ecadb68941af9e00aa931227f07c61a74825b
Henry Walshaw <henry.walshaw@gmail.com>
2018-11-20 02:29:02 +1100

    Better nesting for the voting centres and the districts

--- geojson_service/serialisers.py
+++ geojson_service/serialisers.py
@@ -1,18 +1,35 @@
+from marshmallow import fields
+
 from .serializer_utils import MarshmallowGeoJSON, ma
 from .models import VotingCentre, District
 
 
-class VotingCentreGJ(MarshmallowGeoJSON, ma.ModelSchema):
+class VotingCentreTable(ma.ModelSchema):
     class Meta:
         model = VotingCentre
+
+
+class DistrictTable(ma.ModelSchema):
+    class Meta:
+        model = District
+
+
+class VotingCentreGJ(MarshmallowGeoJSON, ma.ModelSchema):
+
+    class Meta:
+        model = VotingCentre
+
+    districts = fields.Nested(DistrictTable, only='name', many=True, dump_only=True)
 
 
 class DistrictGJ(MarshmallowGeoJSON, ma.ModelSchema):
     class Meta:
         model = District
 
+    voting_centres = fields.Nested(VotingCentreTable, only=('venue_name'), many=True, dump_only=True)
+
 
 voting_centre_collection = VotingCentreGJ(many=True)
-voting_centre_feature = VotingCentre()
+voting_centre_feature = VotingCentreGJ()
 district_collection = DistrictGJ(many=True)
 district_feature = DistrictGJ()
